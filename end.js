7e9b2bf527f16e5b3bc2ea4c5bc37ff9c618c575
Frazier <mrfrazier628@hotmail.com>
2025-12-29 13:12:25 -0500

    updated score function

--- end.js
+++ end.js
@@ -6,13 +6,11 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
-console.log(highScores);
 
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
-        
 })
 
 saveHighScore = (e) => {
@@ -28,6 +26,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
     
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/");
-    console.log(highScores);    
-}
+    window.location.assign("./");  
+};
